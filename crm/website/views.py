8031d240268055b0ad8a8092308edeae738d5ded
Brian Cerda <cerdabrian26@gmail.com>
2023-10-12 12:16:54 -0500

    Created login page.

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+        else:
+            messages.success(
+                request, "There was an error logging in, please try again."
+            )
     return render(request, "home.html")
